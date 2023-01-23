0c0700ef4004722bf2ceca54e39b17c6e8f19ec4
aFku <zapasowyrowerek@gmail.com>
2023-01-23 13:12:53 +0100

    Init repo

---
+++ Instodramat/src/main/java/com/rcbg/afku/instodramat/InstodramatApplication.java
@@ -0,0 +1,13 @@
+package com.rcbg.afku.instodramat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstodramatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstodramatApplication.class, args);
+    }
+
+}
---
+++ Instodramat/src/test/java/com/rcbg/afku/instodramat/InstodramatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcbg.afku.instodramat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstodramatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
